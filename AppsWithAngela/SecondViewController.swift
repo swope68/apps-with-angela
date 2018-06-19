19244110c97ed265cc75efeeaf1a2b71d981eefd
Todd Swope <swope68@gmail.com>
2018-06-18 19:22:27 -0600

    Initial Commit

---
+++ AppsWithAngela/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AppsWithAngela
+//
+//  Created by Todd Swope on 6/18/18.
+//  Copyright © 2018 Todd Swope. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
